19b639db30369e303331ac1d71697427270fb220
angegoua <ange.goua01@gmail.com>
2023-11-04 02:49:03 +0100

    feat: first commit & ini with routes

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,5 +1,5 @@
-import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
+import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline'
+import clsx from 'clsx'
 
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
@@ -9,21 +9,26 @@ export default function InvoiceStatus({ status }: { status: string }) {
         {
           'bg-gray-100 text-gray-500': status === 'pending',
           'bg-green-500 text-white': status === 'paid',
-        },
+        }
       )}
     >
+      <span
+        className={`inline-flex items-center rounded-full px-2 py-1 text-xs ${
+          status === 'pending' && 'bg-gray-100 text-gray-500'
+        }`}
+      ></span>
       {status === 'pending' ? (
         <>
           Pending
-          <ClockIcon className="ml-1 w-4 text-gray-500" />
+          <ClockIcon className='ml-1 w-4 text-gray-500' />
         </>
       ) : null}
       {status === 'paid' ? (
         <>
           Paid
-          <CheckIcon className="ml-1 w-4 text-white" />
+          <CheckIcon className='ml-1 w-4 text-white' />
         </>
       ) : null}
     </span>
-  );
+  )
 }
